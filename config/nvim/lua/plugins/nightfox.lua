93d18c5d0e6ae3b145a8467045844012a75fb9b8
mw3391 <mw3391@columbia.edu>
2024-09-21 17:31:27 -0700

    update nightfox and add solarized

--- config/nvim/lua/plugins/nightfox.lua
+++ config/nvim/lua/plugins/nightfox.lua
@@ -3,21 +3,22 @@
 return {
   {
     'EdenEast/nightfox.nvim',
+    lazy = false,
     priority = 1000,            -- load this before all the other start plugins
+    opts = {
+      options = {
+        styles = {
+          comments = "italic",
+          keywords = "bold",
+          types = "italic,bold",
+        }
+      }
+    },
     init = function()
+      vim.opt.termguicolors = true
+      vim.opt.background = 'dark'
       -- nightfox, dayfox, dawnfox, duskfox, nordfox, terafox, carbonfox
       vim.cmd.colorscheme('duskfox')
-    end,
-    config = function()
-      require('nightfox').setup({
-        options = {
-          styles = {
-            comments = "italic",
-            keywords = "bold",
-            types = "italic,bold",
-          }
-        },
-      })
     end,
   },
 }
