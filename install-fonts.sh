33d0214c6641487c3867a00f06e4360c687840cd
mw3391 <mw3391@columbia.edu>
2024-09-22 15:16:19 -0700

    stub in install fonts

---
+++ install-fonts.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+fonts_dir="fonts"
+mkdir -p "${fonts_dir}"
+
+function download() {
+    local font="${1}"
+    local url="${2}"
+    curl --silent --show-error --location "${url}" --output "${fonts_dir}/${font}.zip"
+    if [ $? -eq 0 ]; then
+        echo "Installed ${font} iterm theme"
+    else
+        echo "Error installing ${font} from ${url}"
+    fi
+}
+
+fonts = (
+    "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Meslo.zip",
+    "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Monaspace.zip",
+)
+
+# todo: actually automate installation
+for f_url in ${fonts}"; do
+    echo "Install font from ${f_url}"
+done
