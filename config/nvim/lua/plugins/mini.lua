0be7438c0dfc0d882fc43786c7517f9f4c76b396
mw3391 <mw3391@columbia.edu>
2024-09-23 22:37:56 -0700

    fix percent

--- config/nvim/lua/plugins/mini.lua
+++ config/nvim/lua/plugins/mini.lua
@@ -22,7 +22,7 @@ return {
 
 			---@diagnostic disable-next-line: duplicate-set-field
 			statusline.section_location = function()
-				return "%2l:%-2v%p%%"
+				return "%2l:%-2v%3p%%"
 			end
 
 			---@diagnostic disable-next-line: duplicate-set-field
