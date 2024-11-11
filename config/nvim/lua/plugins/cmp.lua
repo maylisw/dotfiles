2074cea464b64b34e0d2d7038d7bafa5cbcfa37f
Maÿlis Whetsel <mwhetsel@stripe.com>
2024-11-10 23:05:44 -0800

    update fzf config and add more cmp

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -77,6 +77,25 @@ return {
 					{ name = "copilot" },
 				},
 			})
+
+			-- use buffer source for searching
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+
+			-- Use cmdline & path source for ':'
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline" },
+				}),
+				matching = { disallow_symbol_nonprefix_matching = false },
+			})
 		end,
 	},
 }
--- config/nvim/lua/plugins/fzf.lua
+++ config/nvim/lua/plugins/fzf.lua
@@ -12,15 +12,29 @@ return {
 			end
 		end,
 		keys = {
+			{ "<LEADER>fv", "<cmd>FzfLua help_tags<CR>", desc = "[F]ind [V]im help_tags" },
+			{ "<LEADER>km", "<cmd>FzfLua keymaps<CR>", desc = "[K]ey[M]aps" },
 			{
-				"<LEADER>lg",
+				"<LEADER>ff",
 				function()
-					local fzf = require("fzf-lua")
-					local fzf_lg = require("extensions.fzf_live_grep")
-					fzf.fzf_live(fzf_lg.search, fzf_lg.opts())
+					require("fzf-lua").files({ cwd = vim.fn.getcwd() })
 				end,
-				desc = "[L]ive[G]rep",
+				desc = "Find files",
 			},
+			{ "<LEADER>fw", "<cmd>FzfLua grep<CR>", desc = "[F]ind [W]ords" },
+			{ "<LEADER>fd", "<cmd>FzfLua diagnostics_document<CR>", desc = "[F]ind [D]iagnostics" },
+			{ "<LEADER>fs", "<cmd>FzfLua lsp_document_symbols<CR>", desc = "[F]ind [S]ymbols" },
+			{ "<LEADER>fb", "<cmd>FzfLua git_branches<CR>", desc = "[F]ind [B]ranches" },
+			-- TODO: integrate w/ lg
+			-- {
+			-- 	"<LEADER>lg",
+			-- 	function()
+			-- 		local fzf = require("fzf-lua")
+			-- 		local fzf_lg = require("extensions.fzf_live_grep")
+			-- 		fzf.fzf_live(fzf_lg.search, fzf_lg.opts())
+			-- 	end,
+			-- 	desc = "[L]ive[G]rep",
+			-- },
 			{
 				"<LEADER>fh",
 				function()
@@ -28,41 +42,12 @@ return {
 				end,
 				desc = "[F]ind [H]istory",
 			},
-			{ "<LEADER>fw", "<cmd>FzfLua grep<CR>", desc = "[F]ind [W]ords" },
-			{
-				"<LEADER>fb",
-				"<cmd>FzfLua git_branches<CR>",
-				desc = "[F]ind [B]ranches",
-			},
-			{
-				"<LEADER>fs",
-				"<cmd>FzfLua lsp_document_symbols<CR>",
-				desc = "[F]ind [S]ymbols",
-			},
-			{
-				"<LEADER>fd",
-				"<cmd>FzfLua diagnostics_document<CR>",
-				desc = "[F]ind [D]iagnostics",
-			},
-			{ "<LEADER>km", "<cmd>FzfLua keymaps<CR>", desc = "[K]ey[M]aps" },
-			{
-				"<LEADER>fv",
-				"<cmd>FzfLua help_tags<CR>",
-				desc = "[F]ind [V]im help_tags",
-			},
 			{
 				"<C-p>",
 				function()
 					require("fzf-lua").git_files({ cwd = vim.fn.getcwd() })
 				end,
 				desc = "Find files in git repo",
-			},
-			{
-				"<LEADER>ff",
-				function()
-					require("fzf-lua").files({ cwd = vim.fn.getcwd() })
-				end,
-				desc = "Find files",
 			},
 		},
 	},
