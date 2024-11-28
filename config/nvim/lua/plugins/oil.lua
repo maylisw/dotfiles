2ddbfbeaf57b3b52955d3b265fc7f39ee20e10d7
Maÿlis Whetsel <maylisw31@gmail.com>
2024-11-27 17:13:08 -0800

    whoops forgot to add oil

---
+++ config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,12 @@
+-- [[ oil.lua ]] --
+
+return {
+	{
+		"stevearc/oil.nvim",
+		---@module 'oil'
+		---@type oil.SetupOpts
+		opts = {},
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+}
+-- vim: ts=2 sts=2 sw=2 et
