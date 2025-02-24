2b8a02872a1f025e2f1087ffdb742502381cfc5f
Maÿlis Whetsel <mwhetsel@stripe.com>
2025-02-24 11:29:00 -0800

    fix zshrc to not have output

--- host-st-mwhetsel1/util/host.zsh
+++ host-st-mwhetsel1/util/host.zsh
@@ -12,6 +12,6 @@ export SRC_ACCESS_TOKEN=sgp_foobar
 export SRC_ENDPOINT="http://sourcegraph.corp.stripe.com"
 export SRC_PROXY="unix://~/.stripeproxy"
 
-ssh-add --apple-use-keychain ~/.ssh/id_rsa_mwhetsel@stripe.com
+ssh-add --apple-use-keychain ~/.ssh/id_rsa_mwhetsel@stripe.com > /dev/null 2>&1
 
 source ~/.util/stripe.zsh
